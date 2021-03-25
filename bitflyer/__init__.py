cadbcc9df9bda74f5a1eac5ceae957e97655d76d
Akira Kakkar <akira.kakar@gmail.com>
2021-03-25 01:39:56 +0000

    Add lambda wrapper and devcontainer for python

---
+++ bitflyer/__init__.py
@@ -0,0 +1,34 @@
+import os
+
+from .api import BitflyerAPI, SendNewOrderRequestBody
+
+
+def lambda_handler(event, context):
+    api_key = os.environ["key"]
+    api_secret = os.environ["secret"]
+    product_code = os.environ["product"]
+    child_order_type = os.environ["order_type"]  # 'LIMIT' or 'MARKET'
+    side = os.environ["side"]  # 'BUY' or 'SELL'
+    try:
+        price = int(os.environ["price"])
+        size = float(os.environ["size"])
+    except ValueError:
+        return get_bad_request(400)
+
+    bitflyer = BitflyerAPI(api_key, api_secret)
+    send_order_request = SendNewOrderRequestBody(
+        product_code, child_order_type, side, price, size
+    )
+
+    try:
+        bitflyer.send_new_order(send_order_request)
+        return {
+            "statusCode": 200,
+            "headers": {"Content-Type": "application/json"},
+        }
+    except RuntimeError:
+        return get_bad_request(500)
+
+
+def get_bad_request(status_code: int):
+    return {"statusCode": status_code}
