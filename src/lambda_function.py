c6e5d27535e3389fdd37514433e37fa0bfbf7466
boolafish <boolafish945@gmail.com>
2021-04-04 15:26:23 +0900

    fix: for code review
    - remove try/catch blocks. lambda would fail and exit with log anyway.
    - hardcode side to BUY as we only need the functionality to buy

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -10,14 +10,11 @@ def lambda_handler(event, context):
     api_secret = os.environ["secret"]
 
     child_order_type = "MARKET"  # only handle market order case
+    side = "BUY"  # only BUY and HODL!
     price = 0  # hardcode to 0 for market order
 
-    try:
-        product_code = os.environ["product"]  # 'ETH_JPY or 'BTC_JPY'
-        side = os.environ["side"]  # 'BUY' or 'SELL'
-        target_cost = int(os.environ["target_cost"])
-    except ValueError as err:
-        return get_bad_request(400, repr(err))
+    product_code = os.environ["product"]  # 'ETH_JPY or 'BTC_JPY'
+    target_cost = int(os.environ["target_cost"])
 
     bitflyer = BitflyerAPI(api_key, api_secret)
 
@@ -25,29 +22,21 @@ def lambda_handler(event, context):
     # Use 6 for convinience to round up
     size_precision = 6
 
-    try:
-        ticker_resp = bitflyer.get_ticker(product_code)
-        ticker = ticker_resp.json()
+    ticker_resp = bitflyer.get_ticker(product_code)
+    ticker = ticker_resp.json()
 
-        last_tick_price = ticker["ltp"]  # last tick price
-        size = float(Decimal(target_cost) / Decimal(last_tick_price))
-        precision_adjusted_size = float(
-            Decimal(math.floor(size * 10 ** size_precision))
-            / Decimal(10 ** size_precision)
-        )
+    last_tick_price = ticker["ltp"]  # last tick price
+    size = float(Decimal(target_cost) / Decimal(last_tick_price))
+    precision_adjusted_size = float(
+        Decimal(math.floor(size * 10 ** size_precision)) / Decimal(10 ** size_precision)
+    )
 
-        send_order_request = SendNewOrderRequestBody(
-            product_code, child_order_type, side, price, precision_adjusted_size
-        )
-        resp = bitflyer.send_new_order(send_order_request)
-        return {
-            "statusCode": resp.status_code,
-            "headers": {"Content-Type": "application/json"},
-            "data": resp.json(),
-        }
-    except RuntimeError:
-        return get_bad_request(500)
-
-
-def get_bad_request(status_code: int, reason: str = ""):
-    return {"statusCode": status_code, "reason": reason}
+    send_order_request = SendNewOrderRequestBody(
+        product_code, child_order_type, side, price, precision_adjusted_size
+    )
+    resp = bitflyer.send_new_order(send_order_request)
+    return {
+        "statusCode": resp.status_code,
+        "headers": {"Content-Type": "application/json"},
+        "data": resp.json(),
+    }
