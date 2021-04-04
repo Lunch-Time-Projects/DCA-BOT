ae632c66d39aaffe7b93f4cc01b0db6c170819e4
boolafish <boolafish945@gmail.com>
2021-04-04 15:31:07 +0900

    feat: target cost to size conversion (#10)
    * docs: add instruction for README
    
    * feat: support target_cost to size auto conversion
    
    * fix: style check
    
    * fix: for code review
    
    - remove try/catch blocks. lambda would fail and exit with log anyway.
    - hardcode side to BUY as we only need the functionality to buy

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,4 +1,6 @@
+import math
 import os
+from decimal import Decimal
 
 from bitflyer.api import BitflyerAPI, SendNewOrderRequestBody
 
@@ -6,29 +8,35 @@ from bitflyer.api import BitflyerAPI, SendNewOrderRequestBody
 def lambda_handler(event, context):
     api_key = os.environ["key"]
     api_secret = os.environ["secret"]
-    product_code = os.environ["product"]
-    child_order_type = os.environ["order_type"]  # 'LIMIT' or 'MARKET'
-    side = os.environ["side"]  # 'BUY' or 'SELL'
-    try:
-        price = int(os.environ["price"])
-        size = float(os.environ["size"])
-    except ValueError:
-        return get_bad_request(400)
+
+    child_order_type = "MARKET"  # only handle market order case
+    side = "BUY"  # only BUY and HODL!
+    price = 0  # hardcode to 0 for market order
+
+    product_code = os.environ["product"]  # 'ETH_JPY or 'BTC_JPY'
+    target_cost = int(os.environ["target_cost"])
 
     bitflyer = BitflyerAPI(api_key, api_secret)
-    send_order_request = SendNewOrderRequestBody(
-        product_code, child_order_type, side, price, size
+
+    # Bitflyer only allow to send size of ETH with unit of 6 and BTC of 7
+    # Use 6 for convinience to round up
+    size_precision = 6
+
+    ticker_resp = bitflyer.get_ticker(product_code)
+    ticker = ticker_resp.json()
+
+    last_tick_price = ticker["ltp"]  # last tick price
+    size = float(Decimal(target_cost) / Decimal(last_tick_price))
+    precision_adjusted_size = float(
+        Decimal(math.floor(size * 10 ** size_precision)) / Decimal(10 ** size_precision)
     )
 
-    try:
-        bitflyer.send_new_order(send_order_request)
-        return {
-            "statusCode": 200,
-            "headers": {"Content-Type": "application/json"},
-        }
-    except RuntimeError:
-        return get_bad_request(500)
-
-
-def get_bad_request(status_code: int):
-    return {"statusCode": status_code}
+    send_order_request = SendNewOrderRequestBody(
+        product_code, child_order_type, side, price, precision_adjusted_size
+    )
+    resp = bitflyer.send_new_order(send_order_request)
+    return {
+        "statusCode": resp.status_code,
+        "headers": {"Content-Type": "application/json"},
+        "data": resp.json(),
+    }
