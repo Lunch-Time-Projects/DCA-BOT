e3a0cc953424ae7048c7f8392be84f33111e7ee2
boolafish <boolafish945@gmail.com>
2021-04-04 15:06:58 +0900

    fix: style check

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,26 +1,26 @@
-import os
 import math
+import os
+from decimal import Decimal
 
 from bitflyer.api import BitflyerAPI, SendNewOrderRequestBody
-from decimal import getcontext, Decimal
 
 
 def lambda_handler(event, context):
     api_key = os.environ["key"]
     api_secret = os.environ["secret"]
-    
-    child_order_type = "MARKET" # only handle market order case
-    price = 0 # hardcode to 0 for market order
+
+    child_order_type = "MARKET"  # only handle market order case
+    price = 0  # hardcode to 0 for market order
 
     try:
-        product_code = os.environ["product"] # 'ETH_JPY or 'BTC_JPY'
+        product_code = os.environ["product"]  # 'ETH_JPY or 'BTC_JPY'
         side = os.environ["side"]  # 'BUY' or 'SELL'
         target_cost = int(os.environ["target_cost"])
-    except ValueError:
+    except ValueError as err:
         return get_bad_request(400, repr(err))
 
     bitflyer = BitflyerAPI(api_key, api_secret)
-    
+
     # Bitflyer only allow to send size of ETH with unit of 6 and BTC of 7
     # Use 6 for convinience to round up
     size_precision = 6
@@ -29,10 +29,11 @@ def lambda_handler(event, context):
         ticker_resp = bitflyer.get_ticker(product_code)
         ticker = ticker_resp.json()
 
-        last_tick_price = ticker["ltp"] # last tick price
+        last_tick_price = ticker["ltp"]  # last tick price
         size = float(Decimal(target_cost) / Decimal(last_tick_price))
         precision_adjusted_size = float(
-            Decimal(math.floor(size * 10 ** 6)) / Decimal(10**6)
+            Decimal(math.floor(size * 10 ** size_precision))
+            / Decimal(10 ** size_precision)
         )
 
         send_order_request = SendNewOrderRequestBody(
@@ -42,7 +43,7 @@ def lambda_handler(event, context):
         return {
             "statusCode": resp.status_code,
             "headers": {"Content-Type": "application/json"},
-            "data": resp.json()
+            "data": resp.json(),
         }
     except RuntimeError:
         return get_bad_request(500)
