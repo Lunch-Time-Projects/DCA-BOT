d56addc94e41b823a05080522f0932e42427a065
boolafish <boolafish945@gmail.com>
2021-04-04 15:03:27 +0900

    feat: support target_cost to size auto conversion

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,34 +1,52 @@
 import os
+import math
 
 from bitflyer.api import BitflyerAPI, SendNewOrderRequestBody
+from decimal import getcontext, Decimal
 
 
 def lambda_handler(event, context):
     api_key = os.environ["key"]
     api_secret = os.environ["secret"]
-    product_code = os.environ["product"]
-    child_order_type = os.environ["order_type"]  # 'LIMIT' or 'MARKET'
-    side = os.environ["side"]  # 'BUY' or 'SELL'
+    
+    child_order_type = "MARKET" # only handle market order case
+    price = 0 # hardcode to 0 for market order
+
     try:
-        price = int(os.environ["price"])
-        size = float(os.environ["size"])
+        product_code = os.environ["product"] # 'ETH_JPY or 'BTC_JPY'
+        side = os.environ["side"]  # 'BUY' or 'SELL'
+        target_cost = int(os.environ["target_cost"])
     except ValueError:
-        return get_bad_request(400)
+        return get_bad_request(400, repr(err))
 
     bitflyer = BitflyerAPI(api_key, api_secret)
-    send_order_request = SendNewOrderRequestBody(
-        product_code, child_order_type, side, price, size
-    )
+    
+    # Bitflyer only allow to send size of ETH with unit of 6 and BTC of 7
+    # Use 6 for convinience to round up
+    size_precision = 6
 
     try:
-        bitflyer.send_new_order(send_order_request)
+        ticker_resp = bitflyer.get_ticker(product_code)
+        ticker = ticker_resp.json()
+
+        last_tick_price = ticker["ltp"] # last tick price
+        size = float(Decimal(target_cost) / Decimal(last_tick_price))
+        precision_adjusted_size = float(
+            Decimal(math.floor(size * 10 ** 6)) / Decimal(10**6)
+        )
+
+        send_order_request = SendNewOrderRequestBody(
+            product_code, child_order_type, side, price, precision_adjusted_size
+        )
+        resp = bitflyer.send_new_order(send_order_request)
         return {
-            "statusCode": 200,
+            "statusCode": resp.status_code,
             "headers": {"Content-Type": "application/json"},
+            "data": resp.json()
         }
     except RuntimeError:
         return get_bad_request(500)
 
 
-def get_bad_request(status_code: int):
-    return {"statusCode": status_code}
+def get_bad_request(status_code: int, reason: str = ""):
+    return {"statusCode": status_code, "reason": reason}
